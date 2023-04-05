ee4c58d0b870b45baf048e87927e525303bfdd82
Evan Vargas <evanvargas@gmail.com>
2023-04-04 23:28:14 -0500

    Stack

--- Easy/OneArray.js
+++ Easy/OneArray.js
@@ -99,8 +99,8 @@ function isPalindrome2(str) {
 	const len = str.length;
 	for (let i = 0; i < Math.floor(len / 2); i++) {
 		//iteration 1    r !== s : True
-        //iteration 2    a !== a : False
-        //iteration 3    c !== c : False
+		//iteration 2    a !== a : False
+		//iteration 3    c !== c : False
 		if (str[i] !== str[len - 1 - i]) {
 			return false;
 		}
@@ -109,7 +109,6 @@ function isPalindrome2(str) {
 }
 
 console.log(isPalindrome2(str));
-
 
 //Roman to Integer
 
@@ -124,24 +123,100 @@ function romanToInt(s) {
 		['M', 1000],
 	]);
 	let result = 0;
-    for (let i = 0; i < s.length; i++) {
-        //This gets the first index
-        const current = romanToIntMap.get(s[i]);
-        //this gets the next index by adding one
-        const next = romanToIntMap.get(s[i + 1]);
-        //This checks if a next number exists, if so it then compares
-        if (next && next > current) {
-            //this is result = result + (next - current)
-            result += next - current;
-            //since we used the next index we skip it
+	for (let i = 0; i < s.length; i++) {
+		//This gets the first index
+		const current = romanToIntMap.get(s[i]);
+		//this gets the next index by adding one
+		const next = romanToIntMap.get(s[i + 1]);
+		//This checks if a next number exists, if so it then compares
+		if (next && next > current) {
+			//this is result = result + (next - current)
+			result += next - current;
+			//since we used the next index we skip it
 			i++;
-        } else {
-            // we add: result = result + current
+		} else {
+			// we add: result = result + current
 			result += current;
 		}
-    }
-    //the iteration is complete and we return result
+	}
+	//the iteration is complete and we return result
 	return result;
 }
 
 console.log(romanToInt('IVXX'));
+
+//Longest Common Prefix
+
+const longestCommonPrefix = (wordString) => {
+	//If there are no words in an array
+	if (wordString.length === 0) return '';
+
+	//It's more efficient to start backwards
+	let prefix = wordString[0];
+
+	for (let i = 1; i < wordString.length; i++) {
+		while (wordString[i].indexOf(prefix) !== 0) {
+			prefix = prefix.substring(0, prefix.length - 1);
+			if (prefix === '') return '';
+		}
+	}
+
+	return prefix;
+};
+
+const wordString = ['loop', 'loom', 'loot'];
+console.log(longestCommonPrefix(wordString)); // Output: "loo"
+
+//Write a function that takes a string as input and returns the reverse of that string
+
+const mirroredWord = (string) => {
+	if (string.length === 0) return;
+
+	const wordLength = string.length;
+	let returnString = '';
+	for (let i = wordLength; i > 0; i--) {
+		returnString += string.charAt(i - 1);
+	}
+	return returnString;
+};
+
+console.log(mirroredWord('gum'));
+
+//Create a stack
+//Create a class that has a constructor function that defines the initial state
+//Add methods to the class
+
+class Stack {
+	constructor() {
+		this.items = [];
+	}
+
+	push(item) {
+		this.items.push(item);
+	}
+
+	readAll() {
+		return this.items;
+	}
+	//This removes and returns the top item from the stack
+	pop() {
+		return this.items.pop();
+	}
+
+	peek() {
+		return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+    return this.items.length === 0;
+    }
+}
+
+const aStack = new Stack
+console.log(aStack);
+aStack.push(12);
+aStack.push(24)
+aStack.push(32)
+console.log(aStack.peek());
+aStack.pop()
+console.log(aStack.readAll())
